d61f5f59e47d43ed67768b20f0e1825e952995fb
Azuolas3 <ivoskailt@gmail.com>
2023-02-15 16:37:53 +0200

    setup unit testing framework

---
+++ tests/PieceTests.cpp
@@ -0,0 +1,13 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "pieces.h"
+
+TEST_CASE("Piece Construction")
+{
+    ChessEngine::Color pieceColor = ChessEngine::WHITE;
+    ChessEngine::PieceType pieceType = ChessEngine::ROOK;
+
+    ChessEngine::Piece piece = static_cast<ChessEngine::Piece>(pieceColor | pieceType);
+    CHECK(piece == ChessEngine::W_ROOK);
+}
